a4c75d2fb213e8752dfbe328117e4dda20b013f0
mac <mac@MacBook-Pro-mac.local>
2017-11-12 19:23:03 +0300

    add first version

---
+++ TicTacToe++/app/src/main/java/ru/spbau/tictactoe/EntryPoint.java
@@ -0,0 +1,13 @@
+package ru.spbau.tictactoe;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class EntryPoint extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.entry_point);
+    }
+}
