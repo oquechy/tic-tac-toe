1207bc9bb76886cb46f48cd7a3f6ea9c3ab4c9e7
oquechy <vasilliz@gmail.com>
2017-12-03 20:06:44 +0300

    data base doesn't need permissions

--- TicTacToe++/app/src/main/java/ru/spbau/tictactoe/Statistic/DataBase.java
+++ TicTacToe++/app/src/main/java/ru/spbau/tictactoe/Statistic/DataBase.java
@@ -13,45 +13,6 @@ import java.sql.Statement;
 
 public class DataBase extends SQLiteOpenHelper {
 
-    public void write() throws SQLException, ClassNotFoundException {
-        Class.forName("org.sqlite.JDBC");
-
-        try (Connection conn = DriverManager.getConnection(
-                "jdbc:sqlite:ttt-stats.db")) {
-            try (Statement stmt = conn.createStatement()) {
-                stmt.executeUpdate("drop table if exists cities");
-                stmt.executeUpdate("create table cities (id integer, name varchar(50))");
-                stmt.executeUpdate("insert into cities values(1, 'St. Petersburg')");
-                stmt.executeUpdate("insert into cities values(2, 'Moscow')");
-                try (ResultSet rs = stmt.executeQuery("select * from cities")) {
-                    System.out.println("Writing:");
-                    while (rs.next()) {
-                        System.out.println("name = " + rs.getString("name"));
-                        System.out.println("id = " + rs.getInt("id"));
-                    }
-                }
-            }
-        }
-    }
-
-    public ResultSet read() throws SQLException, ClassNotFoundException {
-        Class.forName("org.sqlite.JDBC");
-
-        try (Connection conn = DriverManager.getConnection(
-                "jdbc:sqlite:ttt-stats.db")) {
-            try (Statement stmt = conn.createStatement();
-                 ResultSet rs = stmt.executeQuery( "SELECT * FROM cities" )
-            ) {
-                System.out.println("Reading:");
-                while (rs.next()) {
-                    System.out.println("name = " + rs.getString("name"));
-                    System.out.println("id = " + rs.getInt("id"));
-                }
-                return rs;
-            }
-        }
-    }
-
     public DataBase(Context context) {
         super(context, "ttt-stats", null, 2);
     }
