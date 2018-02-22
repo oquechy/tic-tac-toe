00d7c254bc2df210ef5b1b9f38944bc473925a88
SashaOrlova <sasha.orlova.1998n@mail.ru>
2018-02-22 12:58:37 +0300

    fix records

--- TicTacToe++/app/src/main/java/ru/spbau/tictactoe/RecordActivity.java
+++ TicTacToe++/app/src/main/java/ru/spbau/tictactoe/RecordActivity.java
@@ -2,9 +2,12 @@ package ru.spbau.tictactoe;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 import java.util.ArrayList;
 
+import ru.spbau.tictactoe.Logic.Result.Result;
 import ru.spbau.tictactoe.Statistic.DataBase;
 
 public class RecordActivity extends AppCompatActivity {
@@ -16,13 +19,15 @@ public class RecordActivity extends AppCompatActivity {
         DataBase dataBase = new DataBase(this);
         DataBase.Entry[] records = dataBase.readRecords();
         ArrayList<String> st = new ArrayList<>();
+        st.add("RESULT");
+        st.add("Result Opponent Moves");
         for (DataBase.Entry elem : records) {
-            st.add(Integer.toString(elem.rowNumber) + ".  " + elem.result
-                    + "  " + elem.opponent + "  " + Integer.toString(elem.moves) + "  " + elem.myType);
+            st.add(Integer.toString(elem.rowNumber) + ".  " + ((elem.result == Result.CROSS) ? "WIN" : "LOSE")
+                    + "  " + elem.opponent + "  " + Integer.toString(elem.moves));
         }
-//        ListView listView = (ListView) findViewById(R.id.listView);
-//        ArrayAdapter<String> adapter = new ArrayAdapter<>(this,
-//                R.layout.list_item, st);
-//        listView.setAdapter(adapter);
+        ListView listView = (ListView) findViewById(R.id.listView);
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(this,
+                R.layout.list_item, st);
+        listView.setAdapter(adapter);
     }
 }
